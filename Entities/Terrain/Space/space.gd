b96aadccdce8f6c0958e5fb98b1112c9c471f404
Dalton <daltonfiggdavies@gmail.com>
2025-04-23 12:12:30 +0100

    Add planets getting a random position in space

--- Entities/Terrain/Space/space.gd
+++ Entities/Terrain/Space/space.gd
@@ -3,10 +3,22 @@ extends Node3D
 
 @export var planet_scene: PackedScene
 
+var planet_count := 5
+var spawn_radius := 5000
+
 func _ready():
 	spawn_planets()
 
 func spawn_planets() -> void:
-	var new_planet_instance = planet_scene.instantiate()
-	
-	add_child(new_planet_instance)
+	# Loop up to the amount of planets we want in space
+	for i in range(planet_count):
+		# Ccreate a new instance of the planet scene
+		var planet_instance = planet_scene.instantiate()
+		
+		# generate a new random position
+		var new_position = Vector3(randf_range(-spawn_radius,spawn_radius),randf_range(-spawn_radius, spawn_radius), randf_range(-spawn_radius, spawn_radius))
+
+		planet_instance.position = new_position
+		
+		# add it as a child
+		add_child(planet_instance)
