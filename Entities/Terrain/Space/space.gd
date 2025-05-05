d1641ec6292dbd041fd75546f7bc6ecf9da00501
Dalton <daltonfiggdavies@gmail.com>
2025-05-05 11:12:16 +0100

    Visual tweaks

--- Entities/Terrain/Space/space.gd
+++ Entities/Terrain/Space/space.gd
@@ -6,19 +6,19 @@ extends Node3D
 var planet_count := 5
 var spawn_radius := 5000
 
-func _ready():
-	spawn_planets()
-
-func spawn_planets() -> void:
-	# Loop up to the amount of planets we want in space
-	for i in range(planet_count):
-		# Ccreate a new instance of the planet scene
-		var planet_instance = planet_scene.instantiate()
-		
-		# generate a new random position
-		var new_position = Vector3(randf_range(-spawn_radius,spawn_radius),randf_range(-spawn_radius, spawn_radius), randf_range(-spawn_radius, spawn_radius))
-
-		planet_instance.position = new_position
-		
-		# add it as a child
-		add_child(planet_instance)
+#func _ready():
+	#spawn_planets()
+#
+#func spawn_planets() -> void:
+	## Loop up to the amount of planets we want in space
+	#for i in range(planet_count):
+		## Ccreate a new instance of the planet scene
+		#var planet_instance = planet_scene.instantiate()
+		#
+		## generate a new random position
+		#var new_position = Vector3(randf_range(-spawn_radius,spawn_radius),randf_range(-spawn_radius, spawn_radius), randf_range(-spawn_radius, spawn_radius))
+#
+		#planet_instance.position = new_position
+		#
+		## add it as a child
+		#add_child(planet_instance)
